2a4493f9b9ad1a12284a3f4f7bcedc0ac2d9c95c
krlim417 <krlim417@gmail.com>
2017-11-08 13:50:16 -0800

    Fix bug in the Search Component by binding functions

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,6 +6,8 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   onChange (e) {
@@ -19,11 +21,14 @@ class Search extends React.Component {
   }
 
   render() {
-    return (<div>
-      <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    return (
+      <div>
+        <h4>Add more repos!</h4>
+        Enter a github username:
+        <input value={this.state.terms} onChange={this.onChange} type="text" name="usernameInput"/>
+        <button onClick={this.search}> Add Repos </button>
+      </div>
+    ) 
   }
 }
 
