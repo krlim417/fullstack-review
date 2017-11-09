51c54fab37ddc1bafd2eb48b7172fa0f1c2ae6c4
krlim417 <krlim417@gmail.com>
2017-11-09 14:03:14 -0800

    Obtain user data from GitHub and saves it to the database if not already there

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,31 @@
 const request = require('request');
 const config = require('../config.js');
+const db = require('../database/index.js');
 
-let getReposByUsername = (/* TODO */) => {
+
+let getReposByUsername = (username) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
+  var user = JSON.parse(username);
+  var url = `https://api.github.com/users/${user}/repos`;
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${user}/repos`,
     headers: {
-      'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'User-Agent': 'krlim417',
+      'Authorization': `token ${config.TOKEN}`,
+      'Accept': 'application/vnd.github.v3+json'
     }
   };
 
+  request(options, function(error, response, body) {
+    var parsedBody = JSON.parse(body);
+    if (parsedBody) {
+      db.save(parsedBody);
+    }
+  });
 }
 
 module.exports.getReposByUsername = getReposByUsername;
