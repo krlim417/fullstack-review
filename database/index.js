bba2d6caee21c97222f04408e1203ba0c21c087f
krlim417 <krlim417@gmail.com>
2017-11-09 14:02:06 -0800

    Create repoSchema and saves items to the database

--- database/index.js
+++ database/index.js
@@ -1,16 +1,43 @@
 const mongoose = require('mongoose');
+Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  repoId: {type: Number, index: {unique: true}},
+  username: String,
+  name: String,
+  description: String,
+  userForked: Boolean,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = body => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  body.forEach(repo => {
+
+    var newRepo = {
+      repoId: repo.id,
+      username: repo.owner.login,
+      name: repo.name,
+      description: repo.description,
+      userForked: repo.fork,
+      forks: repo.forks
+    };
+    var repo = new Repo(newRepo);
+
+    repo.save((err, repo) => {
+      if (err) {
+        console.log(`There's already an entry with the same repoId.`);
+      } else {
+        console.log('The data was successfully added to the database.');  
+      }
+    });
+
+  });
 }
 
 module.exports.save = save;
