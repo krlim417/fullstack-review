f37ddd9d4819e6eff97551d5e81047bf6a036dc2
krlim417 <krlim417@gmail.com>
2017-11-09 18:29:25 -0800

    Receive data back from server

--- database/index.js
+++ database/index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
@@ -8,36 +7,43 @@ let repoSchema = mongoose.Schema({
   name: String,
   description: String,
   userForked: Boolean,
-  forks: Number
+  forks: Number,
+  repoUrl: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = body => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-  body.forEach(repo => {
+  if (body) {
+    body.forEach(repo => {
 
-    var newRepo = {
-      repoId: repo.id,
-      username: repo.owner.login,
-      name: repo.name,
-      description: repo.description,
-      userForked: repo.fork,
-      forks: repo.forks
-    };
-    var repo = new Repo(newRepo);
+      var newRepo = {
+        repoId: repo.id,
+        username: repo.owner.login,
+        name: repo.name,
+        description: repo.description,
+        userForked: repo.fork,
+        forks: repo.forks
+      };
+      var repo = new Repo(newRepo);
 
-    repo.save((err, repo) => {
-      if (err) {
-        console.log(`There's already an entry with the same repoId.`);
-      } else {
-        console.log('The data was successfully added to the database.');  
-      }
+      repo.save((err, repo) => {
+        if (err) {
+          console.log(`There's already an entry with the same repoId.`);
+        } else {
+          console.log('The data was successfully added to the database.');  
+        }
+      });
     });
+  };
+};
 
+var get = function(callback) {
+  Repo.find({}, (err, repos) => {
+    callback(repos); 
   });
-}
+};
+  // Repo.find({}).limit(25).sort({forks: -1});
 
 module.exports.save = save;
+module.exports.get = get;
